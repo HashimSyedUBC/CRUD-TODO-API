f23b77073a428f89eb339674bb71ba762a12789d
Hashim Syed <syedhashim2015@gmail.com>
2022-11-12 12:13:40 -0800

    Added error catcher and few more comments.

--- index.js
+++ index.js
@@ -17,4 +17,21 @@ app.get('/', (req, res) => {
     res.send('Home page');
 }) ;
 
+//If none of the above uses are able to handle request, sends it to error handling catch
+app.use((req, res) => {
+    const error = new Error('Not found');
+    error.status(404);
+    next(error);
+})
+
+//Error handling catch. Catches all errors.
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.send({
+        error : {
+            message: error.message
+        }
+    });
+});
+
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
